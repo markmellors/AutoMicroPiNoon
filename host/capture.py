0d879e86fbd620605842fed013b747231b0ac43e
markmellors <mark.mellors@gmail.com>
2019-03-26 21:16:57 +0000

    first attempt at a webstream from opencv, not working

---
+++ host/capture.py
@@ -0,0 +1,57 @@
+'''capture.py'''
+from imutils.video import VideoStream
+import datetime
+import argparse
+import imutils
+import cv2, sys
+import time
+# import the necessary packages
+#from webcamvideostream import WebcamVideoStream
+
+class VideoStream:
+	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
+		framerate=32):
+		# check to see if the picamera module should be used
+		if usePiCamera:
+			# only import the picamera packages unless we are
+			# explicity told to do so -- this helps remove the
+			# requirement of `picamera[array]` from desktops or
+			# laptops that still want to use the `imutils` package
+			from pivideostream import PiVideoStream
+
+			# initialize the picamera stream and allow the camera
+			# sensor to warmup
+			self.stream = PiVideoStream(resolution=resolution,
+				framerate=framerate)
+
+		# otherwise, we are using OpenCV so initialize the webcam
+		# stream
+		else:
+			self.stream = WebcamVideoStream(src=src)
+	# start the threaded video stream
+		return self.stream.start()
+
+	def update(self):
+		# grab the next frame from the stream
+		self.stream.update()
+
+	def read(self):
+		# return the current frame
+		return self.stream.read()
+
+	def stop(self):
+		# stop the thread and release any resources
+		self.stream.stop()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+	help="whether or not the Raspberry Pi camera should be used")
+args = vars(ap.parse_args())
+
+# initialize the video stream
+
+cap = cv2.VideoCapture(usePiCamera=args["picamera"] > 0).start()
+while True :
+    ret, frm = cap.read()
+    sys.stdout.write( frm.tostring() )
